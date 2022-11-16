2819cedf3747257f8ade2775847375b7ef7598aa
3rd.mayconn.lbispo <3rd.mayconn.lbispo@americanas.io>
2022-11-16 12:47:27 -0300

    Configura o morgan para dev

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const testRouter = require('./routes/testRouter');
 
+app.use(express.json());
+app.use(morgan('dev'));
 app.use('/test', testRouter);
 
 module.exports = app;
