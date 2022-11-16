4a29fe9de5d6e0a02512d4ddce8744d9ee2a7755
3rd.mayconn.lbispo <3rd.mayconn.lbispo@americanas.io>
2022-11-16 14:19:50 -0300

    Cria productRoutes e endpoints de Products

---
+++ routes/productRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const res = require('express/lib/response');
+const router = express.Router();
+
+//const products = [];
+
+//Return all products
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        message: "Products returned successfully"
+    });
+});
+
+//Return product by Id
+router.get('/:id', (req, res, next) => {
+    res.status(200).send({
+        message: "Product returned successfully"
+    });
+});
+
+//Add product
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        message: "Product created successfully"
+    });
+});
+
+module.exports = router;
