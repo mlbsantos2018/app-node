d5098735cbd309b27bd528d7db59226743038915
3rd.mayconn.lbispo <3rd.mayconn.lbispo@americanas.io>
2022-11-16 12:48:09 -0300

    Realiza ajustes nas mensagens de das requisições e de Log

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -4,8 +4,14 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
-        message: "Server is Running..."
+        message: "Server OK, No problem found..."
     });
+});
+
+router.post('/', (req, res, next) => {
+    const {nome, idade} = req.body;
+    const pessoa = [nome, idade];
+    res.status(200).send(pessoa);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const http = require('http');
 const app = require('./app');
 const port = 3000;
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Application started on port: ${port}`);
+});
