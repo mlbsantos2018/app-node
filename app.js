9a4509bc2c68a92b15b5ff7a6259606383a573a6
3rd.mayconn.lbispo <3rd.mayconn.lbispo@americanas.io>
2022-11-16 13:11:00 -0300

    Configura mensagens de erro

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const testRouter = require('./routes/testRouter');
+const productRoutes = require();
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+
 app.use('/test', testRouter);
+
+app.use((req, res, next) => {
+    const erro = new Error('Path not found');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
