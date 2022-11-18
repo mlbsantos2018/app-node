a4c0b708ba032500764fe8d8442cb76c95dfa52c
3rd.mayconn.lbispo <3rd.mayconn.lbispo@americanas.io>
2022-11-18 16:59:42 -0300

    Refatora os métodos e implementa a camada de product service

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,146 +1,41 @@
 const express = require('express');
 const res = require('express/lib/response');
+const Product = require('../src/model/Product');
+const ProductService = require('../src/service/ProductService')
+
+
+const productService = new ProductService();
+
 const router = express.Router();
 
-let products = [
-    {
-        "id":123,
-        "name": "Smartphone Xiaomi note 9 pro"
-    },
-    {
-        "id":456,
-        "name": "Smartphone Xiaomi note 7"
-    },
-    {
-        "id":789,
-        "name": "Smartphone Xiaomi note 6"
-    }
-];
-
-
 router.get('/', (req, res, next) => {
-   
-    let responseBody;
-
-    console.log("Searching all products");
-    if(products.length ===0){
-        console.log("No products found");
-        responseBody = {
-            message: "No products found"
-        }
-    
-    }else{
-        console.log("Successfully found products");
-        responseBody = products;
-    }
-    return res.status(200).json(responseBody);
+    let response = productService.getAll()
+    console.log(response.statusCode);
+    return res.status(response.statusCode).json(response.body);
 });
 
 router.get('/:id', (req, res, next) => {
-
-    let responseBody;
-    let statusCode;
-    let productExist = false;
-
-    const productId = req.params.id;
-
-    console.log("Searching all products");
-    products.forEach( product => {
-        if(product.id == productId){
-            console.log("Product found successfully");
-            responseBody = product;
-            productExist = true;
-            statusCode = 200;
-        }
-    });
-
-    if(!productExist){
-        responseBody = { message: "Product not found"};
-        statusCode = 404;
-    }
-    return res.status(statusCode).json(responseBody);
+    let product = new Product(req.params.id, req.body.name, req.body.price);
+    let response = productService.getById(product);
+    return res.status(response.statusCode).json(response.body);
 });
 
 router.post('/', (req, res, next) => {
-    
-    const product = {
-        id: req.body.id,
-        name: req.body.name
-    };
-
-    products.push(product);
-    console.log("Product successfully created");
-
-    return res.status(201).json(product);
-
+    let product = new Product(req.body.id, req.body.name, req.body.price);
+    let response = productService.add(product);
+    return res.status(response.statusCode).json(response.body);
 });
 
 router.delete('/:id', (req, res, next) =>{
-    
-    let responseBody;
-    let statusCode;
-    let productExist = false;
-    const productId = req.params.id;
-
-    console.log("Searching all products");
-    products.forEach( product => {
-        if(product.id == productId){
-            const index = products.indexOf(product);
-            if (index > -1) {
-                products.splice(index, 1);
-                console.log("Product successfully removed");
-                responseBody = {
-                    message: "Product successfully removed"
-                };
-                statusCode = 200;
-                productExist = true;
-            }
-        }
-    });
-
-    if(!productExist){
-        responseBody = {
-            message: "Product not found"
-        }
-        statusCode = 404;
-    }
-
-    return res.status(statusCode).json(responseBody);
+    let product = new Product(req.params.id, req.body.name, req.body.price);
+    let response = productService.delete(product);
+    return res.status(response.statusCode).json(response.body);
 });
 
 router.put('/', (req, res, next) => {
-    let responseBody;
-    let statusCode;
-    let productExist = false;
-
-    console.log("Searching all products");
-    products.forEach( product => {
-        if(product.id == req.body.id){
-
-            const index = products.indexOf(product);
-
-            const productUpdated = {
-                id: req.body.id,
-                name: req.body.name
-            };
-        
-            products[index] = productUpdated;
-            console.log("Product successfully updated");
-
-            responseBody = productUpdated;
-            productExist = true;
-            statusCode = 200;
-        }
-    });
-
-    if(!productExist){
-        responseBody = {
-            message: "Product not found"
-        }
-        statusCode = 404;
-    }
-
-    res.status(statusCode).json(responseBody);
+    let product = new Product(req.body.id, req.body.name, req.body.price);
+    let response = productService.update(product);
+    return res.status(response.statusCode).json(response.body);
 });
 
 module.exports = router;
